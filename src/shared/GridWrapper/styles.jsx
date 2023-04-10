9da94cacbe1f7b2eb7542a1f045bf2933aea1a8b
ankm19 <ankm335@gmail.com>
2023-04-10 11:54:06 +0530

    change

--- src/shared/GridWrapper/styles.jsx
+++ src/shared/GridWrapper/styles.jsx
@@ -1,7 +1,8 @@
+
 export const gridWrapperStyles = {
     position: 'relative',
     padding: '48px 32px',
-    minHeight: 'calc(100vh - 166px)',
-    marginLeft: '320px',
+    minHeight: `calc(${screen.availHeight} - 166px)`,
+    // marginLeft: '0px',
     backgroundColor: '#eaeff1',
 };
